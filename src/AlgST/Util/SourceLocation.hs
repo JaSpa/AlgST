9d253c49da39a9d3a7ad2a891d06781ec89f2520
Janek Spaderna <janek@spaderna.eu>
2023-01-20 17:30:31 +0100

    Functions and typeclasses to extract ranges

--- src/AlgST/Util/SourceLocation.hs
+++ src/AlgST/Util/SourceLocation.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE UnliftedNewtypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module AlgST.Util.SourceLocation
   ( -- * Locations
@@ -13,6 +15,11 @@ module AlgST.Util.SourceLocation
     SrcRange ((:@<), ..),
     unsafeRangeString,
 
+    -- * Extracting ranges
+    HasRange (..),
+    Generically (..),
+    GHasRange,
+
     -- * @ByteString@ interactions
     startLoc,
     fullRange,
@@ -20,15 +27,17 @@ module AlgST.Util.SourceLocation
   )
 where
 
+import AlgST.Util.Generically
 import Data.ByteString qualified as BS
 import Data.ByteString.Internal (ByteString (..))
 import Data.Coerce
 import Data.Hashable
+import Data.Void
 import Data.Word
 import Foreign
 import GHC.Foreign qualified as GHC
 import GHC.ForeignPtr
-import GHC.Generics (Generic)
+import GHC.Generics
 import System.IO qualified as IO
 import System.IO.Unsafe
 
@@ -61,7 +70,7 @@ data SrcRange = SrcRange {rangeStart, rangeEnd :: !SrcLoc}
 
 -- | This instance is only meant for debugging purposes. The output
 -- unconditionally contains colorizing escape sequences (see the @Show
--- 'SrcLoc'@ instance documenttion for more information).
+-- 'SrcLoc'@ instance documentation for more information).
 instance Show SrcRange where
   showsPrec p r =
     showParen (p > 10) do
@@ -74,6 +83,8 @@ instance Hashable SrcRange
 
 pattern (:@<) :: SrcLoc -> SrcLoc -> SrcRange
 pattern a :@< b = SrcRange a b
+
+{-# COMPLETE (:@<) #-}
 
 -- | Counts the number of bytes included in the range.
 rangeByteSize :: SrcRange -> Int
@@ -91,3 +102,41 @@ unsafeRangeString r = unsafeDupablePerformIO do
 
 unsafeBasePtr :: ByteString -> Ptr Word8
 unsafeBasePtr (PS fp offset _) = unsafeForeignPtrToPtr fp `plusPtr` offset
+
+class HasRange a where
+  {-# MINIMAL getRange | getStartLoc, getEndLoc #-}
+
+  getRange :: a -> SrcRange
+  getRange a = getStartLoc a :@< getEndLoc a
+
+  getStartLoc :: a -> SrcLoc
+  getStartLoc = rangeStart . getRange
+
+  getEndLoc :: a -> SrcLoc
+  getEndLoc = rangeEnd . getRange
+
+instance HasRange SrcRange where
+  getRange = id
+
+instance HasRange Void where
+  getRange = absurd
+
+instance (Generic a, GHasRange (Rep a)) => HasRange (Generically a) where
+  getRange (Generically a) = getRangeRep (from a)
+
+-- | Extracts the @SrcRange@ from the leftmost component of each constructor.
+class GHasRange f where
+  getRangeRep :: f a -> SrcRange
+
+instance (GHasRange f, GHasRange g) => GHasRange (f :+: g) where
+  getRangeRep (L1 f) = getRangeRep f
+  getRangeRep (R1 g) = getRangeRep g
+
+instance GHasRange f => GHasRange (f :*: g) where
+  getRangeRep (f :*: _) = getRangeRep f
+
+instance GHasRange f => GHasRange (M1 i c f) where
+  getRangeRep (M1 f) = getRangeRep f
+
+instance HasRange c => GHasRange (K1 i c) where
+  getRangeRep (K1 c) = getRange c
